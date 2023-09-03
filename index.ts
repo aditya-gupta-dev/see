3b09745ffeadb8980e6c2f4f65b0fcae8934afaa
aditya-gupta-dev <142041364+aditya-gupta-dev@users.noreply.github.com>
2023-09-03 21:18:53 +0530

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,9 +1,21 @@
 import { listDirectoriesSync } from './utils';
 
-function main(directory: string) {
-        
-    listDirectoriesSync(directory)
-    .map((val) => console.log(val));
+function print(object: any) {
+    console.log(object);
 }
 
-main(".");
+function main(directory: string) {
+        
+    const directories: string[] = listDirectoriesSync(directory);
+    
+    if(directories.length < 1) {
+        console.log('there should be at least two directories...');
+    }
+    else {
+        const firstTwoDirectories = directories.slice(0, 2);
+        print(`Comparing <${firstTwoDirectories[0]}> and <${firstTwoDirectories[1]}>...`);
+        
+    }
+}
+
+main("H:/Typescript/see");
