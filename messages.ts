23a74c2a27ede90e97b2fea3246d669eec60fd02
aditya-gupta-dev <142041364+aditya-gupta-dev@users.noreply.github.com>
2023-09-06 23:44:49 +0530

    Create messages.ts

---
+++ messages.ts
@@ -0,0 +1,5 @@
+export const messages = {
+    numberOfFilesNotEqual : (filesInFirstDirectory: number, filesInSecondDirectory: number) => 
+        `F: first directory contains ${filesInFirstDirectory} no. of files whereas the second one contains ${filesInSecondDirectory}`
+    
+}
