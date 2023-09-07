896651006ef877a6f754265ba05a5e01df856d35
aditya-gupta-dev <142041364+aditya-gupta-dev@users.noreply.github.com>
2023-09-07 22:56:51 +0530

    Re correcting a mistake

--- index.ts
+++ index.ts
@@ -1,54 +1,22 @@
-import fs from 'fs';
-import path from 'path';
-import { messages } from './messages';
+import { compareTwoDirectories, listDirectoriesSync } from './utils';
 
-function getAllFiles(dirPath: string, arrayOfFiles: string[]): string[] {
-    const files = fs.readdirSync(dirPath);
-  
-    arrayOfFiles = arrayOfFiles || [];
-  
-    files.forEach(function (file) {
-      const filePath = path.join(dirPath, file);
-      if (fs.statSync(filePath).isDirectory()) {
-        arrayOfFiles = getAllFiles(filePath, arrayOfFiles);
-      } else {
-        arrayOfFiles.push(filePath);
-      }
-    });
-  
-    return arrayOfFiles;
-  }
+function print(object: any) {
+    console.log(object);
+}
 
-export async function compareTwoDirectories(first: string, second: string) {
-    const firstFiles = getAllFiles(first, [])
-    const secondFiles = getAllFiles(second, [])
-
-    if(firstFiles.length !== secondFiles.length) {
-        console.log(messages.numberOfFilesNotEqual(firstFiles.length, secondFiles.length))
+function main(directory: string) {
+        
+    const directories: string[] = listDirectoriesSync(directory);
+    
+    if(directories.length < 1) {
+        print('there should be at least two directories...');
     }
     else {
-      console.log(messages.numberOfFilesEqual(firstFiles.length));
-      for(let i = 0; i < firstFiles.length; i++) {
-        const baseOfFirst = path.basename(firstFiles[i]);
-        const baseOfSecond = path.basename(secondFiles[i])
-
-      }
+        const firstTwoDirectories = directories.slice(0, 2);
+        
+        print(`Comparing <${firstTwoDirectories[0]}> and <${firstTwoDirectories[1]}>...`);
+        compareTwoDirectories(firstTwoDirectories[0], firstTwoDirectories[1])
     }
 }
 
-export function listDirectoriesSync(dirPath: string): string[] {
-    const directories: string[] = [];
-
-    const entities: string[] = fs.readdirSync(dirPath);
-
-    entities.forEach((entity: string) => {
-
-        const absolutePath = path.join(dirPath, entity);
-
-        if(fs.statSync(absolutePath).isDirectory()) {
-            directories.push(absolutePath);
-        }
-    });
-
-    return directories;
-}
+main("H:/Typescript/see");
