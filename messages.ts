d14191d614f1412f83a418338769a9bf6d65df5f
aditya-gupta-dev <142041364+aditya-gupta-dev@users.noreply.github.com>
2023-09-07 22:39:16 +0530

    Update messages.ts

--- messages.ts
+++ messages.ts
@@ -1,5 +1,8 @@
 export const messages = {
     numberOfFilesNotEqual : (filesInFirstDirectory: number, filesInSecondDirectory: number) => 
-        `F: first directory contains ${filesInFirstDirectory} files whereas the second one contains ${filesInSecondDirectory} files`
+        `F: first directory contains ${filesInFirstDirectory} files whereas the second one contains ${filesInSecondDirectory} files`,
+    numberOfFilesEqual : (filesInFirstDirectory: number) => 
+        `P: first and the second directory contain the same no. of files i.e. ${filesInFirstDirectory}`,
+    
     
 }
