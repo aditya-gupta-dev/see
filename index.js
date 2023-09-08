7d3656f92db6c64ca48eae8da66e29f61a8b6013
aditya-gupta-dev <142041364+aditya-gupta-dev@users.noreply.github.com>
2023-09-08 22:17:54 +0530

    Update index.js with latest compilation

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const utils_1 = require("./utils");
-function main(directory) {
-    (0, utils_1.listDirectories)(directory)
-        .map((val) => console.log(val));
+function print(object) {
+    console.log(object);
 }
-main(".");
+function main(directory) {
+    const directories = (0, utils_1.listDirectoriesSync)(directory);
+    if (directories.length < 1) {
+        print('there should be at least two directories...');
+    }
+    else {
+        const firstTwoDirectories = directories.slice(0, 2);
+        print(`Comparing <${firstTwoDirectories[0]}> and <${firstTwoDirectories[1]}>...`);
+        (0, utils_1.compareTwoDirectories)(firstTwoDirectories[0], firstTwoDirectories[1]);
+    }
+}
+main("H:/Typescript/see");
